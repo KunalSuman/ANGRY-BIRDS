a3b63d35b56c5f6bf1b01b743b9bdb94940155ed
JaganKumarTata <147944141+ProLolGod@users.noreply.github.com>
2024-10-31 14:17:06 +0530

    Update Level_selector.java

--- core/src/main/java/io/github/KunalSuman/Angry_Bird/Levels/Level_selector.java
+++ core/src/main/java/io/github/KunalSuman/Angry_Bird/Levels/Level_selector.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import io.github.KunalSuman.Angry_Bird.Button;
 import io.github.KunalSuman.Angry_Bird.Main;
 import io.github.KunalSuman.Angry_Bird.Menu_page;
 import io.github.KunalSuman.Angry_Bird.Settings_page;
@@ -39,11 +40,14 @@ public class Level_selector extends ScreenAdapter {
     private float l5X,l5Y,l5W,l5H;
     public Stage stage;
     public Table t1 ;
-    public Texture Load_button1 ;
-    public Texture Load_button2 ;
+    public Texture load_button1 ;
+    public Texture load_button2 ;
     public TextButton b2 ;
     public OrthographicCamera camera ;
     public OrthogonalTiledMapRenderer renderer ;
+    public Texture ts ;
+    ImageButton Load_button1;
+    ImageButton Load_button2;
 
     public Level_selector(Main main) {
         this.main = main;
@@ -52,27 +56,28 @@ public class Level_selector extends ScreenAdapter {
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         backButtonTexture = new Texture("backButton.png");
-        Load_button1 = new Texture("Load_button.png");
-        Load_button2 = new Texture("Load_button.png");
+        load_button1 = new Texture("Load_button.png");
+        load_button2 = new Texture("Load_button.png");
+        ts = new Texture("backButton.png");
         this.t1 = new Table();
         camera = new OrthographicCamera();
         renderer = new OrthogonalTiledMapRenderer(new TmxMapLoader().load("LEVEL_SELECTOR.tmx"));
         camera.setToOrtho(false, 1920, 1080);
 
-        TextureRegionDrawable drawable_load1 = new TextureRegionDrawable(new TextureRegion(Load_button1));
+        TextureRegionDrawable drawable_load1 = new TextureRegionDrawable(new TextureRegion(load_button1));
         ImageButton.ImageButtonStyle Load_button1Style = new ImageButton.ImageButtonStyle();
         Load_button1Style.up = drawable_load1;
-        ImageButton Load_button1 = new ImageButton(Load_button1Style);
+        Load_button1 = new ImageButton(Load_button1Style);
         stage.addActor(Load_button1);
-        Load_button1.setSize(300,132);
+        Load_button1.setSize(Gdx.graphics.getWidth()/6.4f,Gdx.graphics.getHeight()/8.18181818182f);
         Load_button1.setPosition((Gdx.graphics.getWidth()*0.72f - Load_button1.getWidth()*0.05f), (Gdx.graphics.getHeight()/0.94f - Load_button1.getHeight()*0.69f) / 2f);
 
-        TextureRegionDrawable drawable_load2 = new TextureRegionDrawable(new TextureRegion(Load_button2));
+        TextureRegionDrawable drawable_load2 = new TextureRegionDrawable(new TextureRegion(load_button2));
         ImageButton.ImageButtonStyle Load_button2Style = new ImageButton.ImageButtonStyle();
         Load_button2Style.up = drawable_load2;
-        ImageButton Load_button2 = new ImageButton(Load_button2Style);
+        Load_button2 = new ImageButton(Load_button2Style);
         stage.addActor(Load_button2);
-        Load_button2.setSize(300,132);
+        Load_button2.setSize(Gdx.graphics.getWidth()/6.4f,Gdx.graphics.getHeight()/8.18181818182f);
         Load_button2.setPosition((Gdx.graphics.getWidth()*0.72f - Load_button2.getWidth()*0.05f), Gdx.graphics.getHeight()*0.10f);
 
         Load_button1.addListener(new ClickListener() {
@@ -86,13 +91,21 @@ public class Level_selector extends ScreenAdapter {
             }
         });
 
+        ImageButton back_button = Button.createButton(ts,stage,100,100,0 , 0);
+
+        back_button.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                main.setScreen(new Menu_page(main));
+            }
+        });
         stage.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 float adjustedY = Gdx.graphics.getHeight() - y;
-                if (x>=bbX && x<=bbX+bbW && adjustedY>=bbY && adjustedY<=bbY+bbH){
-                    main.setScreen(new Menu_page(main));
-                }else if (x>=l1X && x<=l1X+l1W && adjustedY>=l1Y && adjustedY<=l1Y+l1H){
+//                if (x>=bbX && x<=bbX+bbW && adjustedY>=bbY && adjustedY<=bbY+bbH){
+//                    main.setScreen(new Menu_page(main));
+//                }
+              if (x>=l1X && x<=l1X+l1W && adjustedY>=l1Y && adjustedY<=l1Y+l1H){
                     main.setScreen(new Level1(main));
                 }else if (x>=l2X && x<=l2X+l2W && adjustedY>=l2Y && adjustedY<=l2Y+l2H){
                     main.setScreen(new Level2(main));
@@ -139,6 +152,11 @@ public class Level_selector extends ScreenAdapter {
         bbH = 100;
         bbX= 0;
         bbY = 0;
+        Load_button1.setSize(Gdx.graphics.getWidth()/6.4f,Gdx.graphics.getHeight()/8.18181818182f);
+        Load_button1.setPosition((Gdx.graphics.getWidth()*0.72f - Load_button1.getWidth()*0.05f), (Gdx.graphics.getHeight()/0.94f - Load_button1.getHeight()*0.69f) / 2f);
+        Load_button2.setSize(Gdx.graphics.getWidth()/6.4f,Gdx.graphics.getHeight()/8.18181818182f);
+        Load_button2.setPosition((Gdx.graphics.getWidth()*0.72f - Load_button2.getWidth()*0.05f), Gdx.graphics.getHeight()*0.10f);
+
         System.out.println((int)(Gdx.graphics.getWidth()/19.2));
         System.out.println((int)(Gdx.graphics.getHeight()/1.96363636364));
         l1X=(int)(Gdx.graphics.getWidth()/26.8);l1Y=(int)(Gdx.graphics.getHeight()/1.86363636364);l1W=100;l1H=100;
@@ -146,5 +164,5 @@ public class Level_selector extends ScreenAdapter {
         l3X=(int)(Gdx.graphics.getWidth()/3.49090909091);l3Y=(int)(Gdx.graphics.getHeight()/2.16);l3W=100;l3H=100;
         l4X=(int)(Gdx.graphics.getWidth()/2.72631578947);l4Y=(int)(Gdx.graphics.getHeight()/2.84210526316);l4W=100;l4H=100;
         l5X=(int)(Gdx.graphics.getWidth()/2.13333333333);l5Y=(int)(Gdx.graphics.getHeight()/1.74210526316);l5W=100;l5H=100;
-}
+    }
 }
