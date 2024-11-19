dd771ce890a6f2357200394a8ae95664760648dd
JaganKumarTata <147944141+ProLolGod@users.noreply.github.com>
2024-11-20 01:01:07 +0530

    Update Black_bird.java

--- core/src/main/java/io/github/KunalSuman/Angry_Bird/Birds/Black_bird.java
+++ core/src/main/java/io/github/KunalSuman/Angry_Bird/Birds/Black_bird.java
@@ -1,10 +1,25 @@
 package io.github.KunalSuman.Angry_Bird.Birds;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import io.github.KunalSuman.Angry_Bird.Levels.Properties;
 
 public class Black_bird extends Birds_schema implements Birds{
-    Black_bird(Texture texture, int hitpoints, int velocity, int ability , String name , int locked) {
-        super(texture, hitpoints, velocity, ability , name , locked);
+    Black_bird(Body body, World world, Texture Red_bird , int velocity , int ability , int hitpoints , String name , int locked) {
+        super(body, world ,Red_bird, hitpoints , velocity , ability , name ,locked);
+    }
+
+    @Override
+    public void createBirdBody(BodyDef bodyDef, BodyDef.BodyType bodyType, float initialX, float initialY, Properties properties) {
+
+    }
+
+    @Override
+    public void createBirdFixture(FixtureDef birdFixture, int radius) {
+
     }
 
     @Override
