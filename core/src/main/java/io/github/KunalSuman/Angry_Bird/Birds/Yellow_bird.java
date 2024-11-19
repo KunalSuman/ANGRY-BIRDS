53dd19d731f3db7871d1f51778ab27bb50436353
JaganKumarTata <147944141+ProLolGod@users.noreply.github.com>
2024-11-20 01:02:01 +0530

    Update Yellow_bird.java

--- core/src/main/java/io/github/KunalSuman/Angry_Bird/Birds/Yellow_bird.java
+++ core/src/main/java/io/github/KunalSuman/Angry_Bird/Birds/Yellow_bird.java
@@ -1,11 +1,27 @@
 package io.github.KunalSuman.Angry_Bird.Birds;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import io.github.KunalSuman.Angry_Bird.Levels.Properties;
 
 public class Yellow_bird extends Birds_schema implements Birds{
-    Yellow_bird(Texture texture , int velocity , int ability , int hitpoints , String name , int locked) {
-        super(texture , hitpoints , velocity , ability , name , locked);
+    Yellow_bird(Body body, World world, Texture Red_bird , int velocity , int ability , int hitpoints , String name , int locked) {
+        super(body, world ,Red_bird, hitpoints , velocity , ability , name ,locked);
     }
+
+    @Override
+    public void createBirdBody(BodyDef bodyDef, BodyDef.BodyType bodyType, float initialX, float initialY, Properties properties) {
+
+    }
+
+    @Override
+    public void createBirdFixture(FixtureDef birdFixture, int radius) {
+
+    }
+
     @Override
     public void bird_ability() {
         this.ability = 4 ;
